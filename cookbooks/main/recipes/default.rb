84584950fbc5ab6666b5d3690124f2b7b20d8b82
Tim Harding <tim.harding@gmail.com>
2015-03-03 21:36:14 +1300

    Includes SSMTP

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -35,7 +35,7 @@
 # include_recipe "eybackup_slave"
 
 #uncomment to run the ssmtp recipe
-#include_recipe "ssmtp"
+include_recipe "ssmtp"
 
 #uncomment to run the sunspot recipe
 # include_recipe "sunspot"
