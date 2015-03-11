5931f64c8582f12d3dafc65ddb0c1c4158586062
Tim Harding <tim.harding@gmail.com>
2015-03-11 19:40:04 +1300

    Links SSMTP from the app into /etc/

--- cookbooks/ssmtp/recipes/default.rb
+++ cookbooks/ssmtp/recipes/default.rb
@@ -5,22 +5,25 @@ require 'pp'
 #
 
 if ['solo', 'app', 'util', 'app_master'].include?(node[:instance_role])
+    
+  Chef::Log.info "The framework environment is #{node[:environment][:framework_env]}"
 
   directory "/etc/ssmtp" do
     recursive true
     action :delete
   end
 
-  directory "/data/ssmtp" do
+  directory "/etc/ssmtp" do
     owner "root"
     group "root"
     mode "0755"
     action :create
-    not_if "test -d /data/ssmtp"
+    not_if "test -d /etc/ssmtp"
   end
 
-  link "/etc/ssmtp" do
-    to '/data/ssmtp'
+  link "/etc/ssmtp/ssmtp.conf" do
+    Chef::Log.info "The framework environment inside link is '#{node[:environment][:framework_env]}'"
+    to "/data/citizentest/current/config/ssmtp/#{node[:environment][:framework_env]}.conf"
   end
 
 end
