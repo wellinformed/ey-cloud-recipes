618f26c7ce746a5e9333c254aec122e29af0cfb2
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2015-03-11 11:30:34 +0100

    cors: fix nginx config

--- cookbooks/nginx_cors_fix/recipes/default.rb
+++ cookbooks/nginx_cors_fix/recipes/default.rb
@@ -10,11 +10,6 @@ if ['app_master', 'app', 'solo'].include?(node[:instance_role])
       source 'location.block.erb'
     end
 
-    execute "add header to ssl conf" do
-      command "sed -i '/error_log \\/var\\/log\\/engineyard\\/nginx\\/#{app}.ssl.error.log notice;/r /tmp/location.block' /etc/nginx/servers/#{app}.ssl.conf;"
-      not_if "grep -q Access-Control-Allow-Origin /etc/nginx/servers/#{app}.ssl.conf"
-    end
-
     execute "add header to conf" do
       command "sed -i '/error_log \\/var\\/log\\/engineyard\\/nginx\\/#{app}.error.log notice;/r /tmp/location.block' /etc/nginx/servers/#{app}.conf"
       not_if "grep -q Access-Control-Allow-Origin /etc/nginx/servers/#{app}.conf"
