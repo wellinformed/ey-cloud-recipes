006423903e31f2618f8e8dbe1dd903ae4c40547d
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2015-04-22 13:52:40 +0200

    cors: try restart nginx from template block

--- cookbooks/nginx_cors_fix/recipes/default.rb
+++ cookbooks/nginx_cors_fix/recipes/default.rb
@@ -9,13 +9,12 @@ if ['app_master', 'app', 'solo'].include?(node[:instance_role])
   node[:applications].each do |app, data|
     template "/tmp/location.block" do
       source 'location.block.erb'
+      notifies :reload, resources(:service => ["nginx"]), :immediately 
     end
 
     execute "add header to conf" do
       command "sed -i '/error_log \\/var\\/log\\/engineyard\\/nginx\\/#{app}.error.log notice;/r /tmp/location.block' /etc/nginx/servers/#{app}.conf"
       not_if "grep -q Access-Control-Allow-Origin /etc/nginx/servers/#{app}.conf"
-
     end
-    notifies :reload, resources(:service => ["nginx"]), :immediately 
   end
 end
