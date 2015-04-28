66f5c8ab176c07764c0d6dfb4c082bae8a3b1d20
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2015-04-28 11:08:23 +0200

    cors: bring back old style adding cors in main config file

--- cookbooks/nginx_cors_fix/recipes/default.rb
+++ cookbooks/nginx_cors_fix/recipes/default.rb
@@ -9,10 +9,13 @@ end
 
 if ['app_master', 'app', 'solo'].include?(node[:instance_role])
   node[:applications].each do |app, data|
-    template "/data/nginx/servers/theorytest/custom.conf" do
+    template "/tmp/location.block" do
       source 'location.block.erb'
-      owner node[:owner_name]
-      group node[:owner]
+    end
+
+    execute "add header to conf" do
+      command "sed -i '/error_log \\/var\\/log\\/engineyard\\/nginx\\/#{app}.error.log notice;/r /tmp/location.block' /etc/nginx/servers/#{app}.conf"
+      not_if "grep -q Access-Control-Allow-Origin /etc/nginx/servers/#{app}.conf"
       notifies :reload, resources(:service => "nginx"), :delayed
     end
   end
