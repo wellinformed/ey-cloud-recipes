2e11846ea2859455a14750752c08de0716f7bce1
Tim Harding <tim.harding@gmail.com>
2015-06-16 23:33:30 +0100

    Enables some Postgres extensions

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -133,13 +133,13 @@
 #include_recipe "postgresql_maintenance"
 
 #enable Extension modules for a given Postgresql database
-# if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
+if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # Extensions that support Postgres >= 9.0
   # postgresql9_autoexplain "dbname"
   # postgresql9_btree_gin "dbname"
   # postgresql9_btree_gist "dbname"
   # postgresql9_chkpass "dbname"
-  # postgresql9_citext "dbname"
+  postgresql9_citext "dbname"
   # postgresql9_cube "dbname"
   # postgresql9_dblink "dbname"
   # postgresql9_dict_int "dbname"
@@ -151,7 +151,7 @@
   # postgresql9_isn "dbname"
   # postgresql9_lo "dbname"
   # postgresql9_ltree "dbname"
-  # postgresql9_pg_trgm "dbname"
+  postgresql9_pg_trgm "icehitman"
   # postgresql9_pgcrypto "dbname"
   # postgresql9_pgrowlocks "dbname"
 
@@ -180,7 +180,7 @@
   # Admin-Level Contribs
   # postgresql9_pg_buffercache "postgres"
   # postgresql9_pg_freespacemap "postgres"
-# end
+end
 
 #uncomment to include the motd customization related to the environment
 #include_recipe "env_motd"
